c8e7bd6a379384e3eb4a6dbd087f7dfb771fd74e
CX33071 <3541053286@qq.com>
2026-03-19 13:47:41 +0800

    note

--- 第60章/示例.c
+++ 第60章/示例.c
@@ -1,5 +1,16 @@
 //迭代型UDP echo服务器
-
+#include <stdio.h>
+#include <sys/socket.h>
+#define SERVICE "echo"
+#define BUF_SIZE 500
+int main(int argc,char *argv[]){
+    int sfd;
+    ssize_t numread;
+    socklen_t addrlen, len;
+    struct sockaddr_storage claddr;
+    char buf[BUF_SIZE];
+    char addrstr[ID_ADDR_STR_LEN];
+}
 //UDP echo客户端程序
 //并发型TCP echo服务器
 //TCP echo客户端程序
